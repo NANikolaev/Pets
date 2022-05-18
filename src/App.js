8a8417247055caa4aab22d53b309a6977f1428f2
NANikolaev <91592401+NANikolaev@users.noreply.github.com>
2022-05-18 18:40:00 +0300

    add register component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {Route, Routes} from "react-router-dom";
 
 import Navigation from "./components/Header/NavigationBar";
 import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
     <main id="site-content">
        <Routes>
          <Route path="/login" element={<Login/>}></Route>
+         <Route path="/register" element={<Register/>}></Route>
        </Routes>
     </main>
 
